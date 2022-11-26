0f8a4f88f83ebad3e5ca39f8da3427c7820889c9
mengjiayu <mengjiayu2021@gmail.com>
2022-11-26 14:02:36 +0000

    reggie redis

--- src/main/java/com/itheima/reggie/controller/UserController.java
+++ src/main/java/com/itheima/reggie/controller/UserController.java
@@ -8,6 +8,7 @@ import com.itheima.reggie.utils.ValidateCodeUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
@@ -24,15 +26,22 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     @PostMapping("/login")
     public R<User> login(@RequestBody Map map,HttpSession session){
         String key = map.get("phone").toString();
-        session.setAttribute(key,map.get("code"));
+        //save code in redis
+        stringRedisTemplate.opsForValue().set(key,map.get("code").toString(),5, TimeUnit.MINUTES);
+
         log.info(map.toString());
         String phone = map.get("phone").toString();
         String code = map.get("code").toString();
-        Object codeInSession = session.getAttribute(key);
-        if(codeInSession != null && codeInSession.equals(code)){
+        //Object codeInSession = session.getAttribute(key);
+        //get code from redis
+        String codeInsession = stringRedisTemplate.opsForValue().get(key);
+        if(codeInsession != null && codeInsession.equals(code)){
             LambdaQueryWrapper<User> queryWrapper = new LambdaQueryWrapper<>();
             queryWrapper.eq(User::getPhone,phone);
             User user = userService.getOne(queryWrapper);
@@ -43,6 +52,8 @@ public class UserController {
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
+            //if the user has logged in successfully, delete from redis
+            stringRedisTemplate.delete(key);
             return R.success(user);
 
         }
